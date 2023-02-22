551d546bb46209f222bfd240f5bff214a6a93577
kabondok1177 <125608943+kabondok1177@users.noreply.github.com>
2023-02-23 10:43:28 +1300

    Add files via upload

---
+++ 01_sandpit.py
@@ -0,0 +1,10 @@
+# Ask users for a number
+get_number= int(input("choose a number? "))
+
+# Multiply the number by 5
+times_five = get_number * 5
+
+# Output the results
+print(get_number * 5)
+answer = f'{get_number} x 5 = {times_five}'
+print(answer)
