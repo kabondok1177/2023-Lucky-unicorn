16674aea7bf3e1589d49f5178acd1312dfa8a504
kabondok1177 <125608943+kabondok1177@users.noreply.github.com>
2023-03-08 12:30:38 +1300

    Add files via upload

--- 06_Rounds_v01.py
+++ 06_Rounds_v01.py
@@ -3,11 +3,24 @@ balance = 5
 
 rounds_played = 0
 
-play_again = input("Press <Enter> to play...")
+play_again = input("Press <Enter> to play..."). lower()
 while play_again == "":
     # increase # of rounds played
     rounds_played += 1
 
-
     # Print round number
     print("*** Round #{} ***".format(rounds_played))
+    balance -= 1
+    print("Balance: ", balance)
+    print()
+
+    if balance < 1:
+        play_again = "xxx"
+        print("Sorry you have run out of money")
+
+    else:
+        play_again = input("Press Enter to play again"
+                           "or 'xxx' to quit")
+
+    print()
+    print("Final balance", balance)
